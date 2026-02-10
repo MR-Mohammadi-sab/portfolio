88c25fc0419337683fc42428a0423d91ddc7feea
MR-mohammadi-sab <asf423796@gmail.com>
2026-02-10 09:38:05 -0800

    add my project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  return <div className="w-full h-screen bg-gray-500 flex justify-center items-center ">
+
+
+    <div className="animate-pulse">
+    <div className="animate-spin w-12 h-12 rounded-full border-6 border-gray-600 border-b-transparent"></div>
+
+    </div>
+  </div>
+}
+
+
+
+
+
+
+
+
+
+
+export default App
